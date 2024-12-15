b85689b653330aa309250b61e6f658aa4f183cea
yklym <yaroslaw.klymko@gmail.com>
2024-12-15 05:54:52 +0200

    fix: fix build err

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,6 +19,7 @@ import { getFilterOptions } from "./common";
 const DEFAULT_SORT_KEY = AvailableDateShortcuts.TODAY;
 
 const HomePage = () => {
+  // eslint-disable-next-line
   const [currentFilters, setCurrentFilters] = useState({
     start: getDateShortcutStart(DEFAULT_SORT_KEY)?.format(
       DATE_FORMATS.API_DATE
@@ -27,7 +28,7 @@ const HomePage = () => {
   });
 
   const { customDateRange, currentShortcut } = getFilterOptions(currentFilters);
-
+  // eslint-disable-next-line
   const [dateRange, setDateRange] = useState<string>(DEFAULT_DATE_RANGE_KEY);
 
   const onSelectedDateChange = ({ start, end }: SelectDateRangeChangeArgs) => {
